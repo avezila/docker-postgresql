f61edb9d5b254a5bef5d550489cbbfd4094a58e1
Dudko Artemiy <thepioh@zoho.com>
2018-03-17 21:36:22 +0300

    fix

--- build.sh
+++ build.sh
@@ -2,5 +2,7 @@
 set -e
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$ROOT"
-
+set -v
 docker build -t avezila/postgresql:4 .
+docker tag avezila/postgresql:4 nirhub.ru:30050/postgresql:4
+
